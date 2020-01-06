a3c062f81ab9dd6dae769da7eb19b2ef3b5a2990
Felipe <felhenao@gmail.com>
2020-01-05 23:08:00 -0500

    simplified the code in the index.js file by refactoring and moving to a separate file the code having to do with the timer tray

---
+++ app/timerTray.js
@@ -0,0 +1,30 @@
+const electron = require('electron');
+const { Tray } = electron;
+
+class TimerTray extends Tray {
+    constructor (iconPath, mainWindow) { 
+        super (iconPath);
+        this.mainWindow = mainWindow;
+        this.on('click', this.onClick.bind(this));
+    }
+
+    onClick(event, bounds) {
+        const { x, y } = bounds; // Click event bounds
+        const { height, width } = this.mainWindow.getBounds();
+        
+        if (this.mainWindow.isVisible()) {
+            this.mainWindow.hide();
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y -height; // Compatibility for Windows so the window shows in the bottom of the screen
+            this.mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height,
+                width
+            })
+            this.mainWindow.show();
+        }
+    }
+}
+
+module.exports = TimerTray;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
+const TimerTray = require('./app/timerTray');
 let mainWindow;
-let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow ({
@@ -16,23 +16,5 @@ app.on('ready', () => {
 
     const iconName = process.platform == 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);//generate a correct path based on the runing operating system
-    tray = new Tray(iconPath);
-
-    tray.on('click', (event, bounds) => {
-        const { x, y } = bounds; // Click event bounds
-        const { height, width } = mainWindow.getBounds();
-        
-        if (mainWindow.isVisible()) {
-            mainWindow.hide();
-        } else {
-            const yPosition = process.platform === 'darwin' ? y : y -height; // Compatibility for Windows so the window shows in the bottom of the screen
-            mainWindow.setBounds({
-                x: x - width / 2,
-                y: yPosition,
-                height,
-                width
-            })
-            mainWindow.show();
-        }
-    })
-})
+    new TimerTray(iconPath, mainWindow);
+});
